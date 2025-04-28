37f08cd6047cd8bebfae8c5aa82259b0fbe137f7
Nguyễn Thị Thanh <147890041+MPIO1002@users.noreply.github.com>
2025-04-28 09:30:12 +0700

    small update

--- ads/src/components/banner-section/banner/desktopbanner.tsx
+++ ads/src/components/banner-section/banner/desktopbanner.tsx
@@ -20,51 +20,57 @@ const DesktopBanner = ({ bannerList, onBannerClick }: DesktopBannerProps) => {
 
   const scrollLeft = () => {
     if (carouselRef.current) {
-      carouselRef.current.scrollBy({ left: -208 * 6, behavior: 'smooth' }); // Scroll by the width of 6 banners
+      carouselRef.current.scrollBy({ left: -208 * 6, behavior: "smooth" }); // Scroll by the width of 6 banners
     }
   };
 
   const scrollRight = () => {
     if (carouselRef.current) {
-      carouselRef.current.scrollBy({ left: 208 * 6, behavior: 'smooth' }); // Scroll by the width of 6 banners
+      carouselRef.current.scrollBy({ left: 208 * 6, behavior: "smooth" }); // Scroll by the width of 6 banners
     }
   };
+
+  const isScrollable = bannerList.length >= 6; // Check if the number of banners is 6 or more
 
   return (
     <div className="relative">
       {/* Left Button */}
-      <button
-        type="button"
-        className="absolute top-0 left-[-80px] z-30 hidden md:flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        onClick={scrollLeft}
-      >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg
-            className="w-4 h-4 text-white rtl:rotate-180"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M5 1 1 5l4 4"
-            />
-          </svg>
-          <span className="sr-only">Previous</span>
-        </span>
-      </button>
+      {isScrollable && (
+        <button
+          type="button"
+          className="absolute top-0 left-[-80px] z-30 hidden md:flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          onClick={scrollLeft}
+        >
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg
+              className="w-4 h-4 text-white rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 1 1 5l4 4"
+              />
+            </svg>
+            <span className="sr-only">Previous</span>
+          </span>
+        </button>
+      )}
 
       {/* Carousel Container */}
       <div
         ref={carouselRef}
-        className="flex items-center h-[500px] overflow-x-auto space-x-4.5 scrollbar-hide"
+        className={`flex items-center h-[500px] overflow-x-auto space-x-4.5 scrollbar-hide ${
+          !isScrollable ? "justify-center" : ""
+        }`}
         style={{
-          scrollbarWidth: 'none', // For Firefox
-          msOverflowStyle: 'none', // For Internet Explorer and Edge
+          scrollbarWidth: "none", // For Firefox
+          msOverflowStyle: "none", // For Internet Explorer and Edge
         }}
       >
         {bannerList.map((banner) => (
@@ -104,30 +110,32 @@ const DesktopBanner = ({ bannerList, onBannerClick }: DesktopBannerProps) => {
       </div>
 
       {/* Right Button */}
-      <button
-        type="button"
-        className="absolute top-0 right-[-80px] z-30 hidden md:flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        onClick={scrollRight}
-      >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg
-            className="w-4 h-4 text-white rtl:rotate-180"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="m1 9 4-4-4-4"
-            />
-          </svg>
-          <span className="sr-only">Next</span>
-        </span>
-      </button>
+      {isScrollable && (
+        <button
+          type="button"
+          className="absolute top-0 right-[-80px] z-30 hidden md:flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          onClick={scrollRight}
+        >
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg
+              className="w-4 h-4 text-white rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m1 9 4-4-4-4"
+              />
+            </svg>
+            <span className="sr-only">Next</span>
+          </span>
+        </button>
+      )}
     </div>
   );
 };
