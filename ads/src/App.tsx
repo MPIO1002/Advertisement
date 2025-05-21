78f3e3cb9e1f80f7af9460aba03c8f9d24db7da2
Nguyễn Thị Thanh <147890041+MPIO1002@users.noreply.github.com>
2025-05-21 14:44:14 +0700

    feature/login: create interface

--- ads/src/App.tsx
+++ ads/src/App.tsx
@@ -2,12 +2,22 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home";
 import AdminBannerPage from "./pages/admin";
+import LoginPage from "./pages/login";
+import ProtectedRoute from "./components/protected-route";
 
 const App: React.FC = () => (
   <Router>
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/admin" element={<AdminBannerPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute>
+            <AdminBannerPage />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   </Router>
 );
